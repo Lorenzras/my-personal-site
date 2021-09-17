3b75098e497f78a11e040e46e43cbc8db2f11e64
Lorenz Ras <lenzras@gmail.com>
2021-09-18 01:14:47 +0900

    fixed footer position

--- src/components/sections/Footer.jsx
+++ src/components/sections/Footer.jsx
@@ -1,57 +1,57 @@
 const Footer = () => (
-  <div>
+  <div className="-mt-16 md:-mt-32">
     <svg
       version="1.1"
       id="Calque_1"
       x="0px"
       y="0px"
-      viewBox="0 0 1300 350 "
-      style={{ enableBackground: 'new 0 0 1300 300' }}
+      viewBox="0 0 1300 450 "
+      style={{ enableBackground: 'new 0 0 1300 450' }}
       xmlSpace="preserve"
     >
 
-      <path className="st1" d="M 0 250 C 600 450 650 100 1300 250 L 1300 350 L 0 350 L 0 250">
+      <path className="st1" d="M 0 250 C 600 450 650 100 1300 250 L 1300 450 L 0 450 L 0 250">
         <animate
           attributeName="d"
           dur="5s"
           begin="1s"
-          values="M 0 250 C 600 450 650 100 1300 250 L 1300 350 L 0 350 L 0 250;
-                M 0 250 C 600 100 650 450 1300 250 L 1300 350 L 0 350 L 0 250;
-        M 0 250 C 600 450 650 100 1300 250 L 1300 350 L 0 350 L 0 250"
+          values="M 0 250 C 600 450 650 100 1300 250 L 1300 450 L 0 450 L 0 250;
+                M 0 250 C 600 100 650 450 1300 250 L 1300 450 L 0 450 L 0 250;
+        M 0 250 C 600 450 650 100 1300 250 L 1300 450 L 0 450 L 0 250"
           repeatCount="indefinite"
         />
       </path>
 
-      <path className="st1" d="M 0 250 C 600 100 650 450 1300 250 L 1300 350 L 0 350 L 0 250">
+      <path className="st1" d="M 0 250 C 600 100 650 450 1300 250 L 1300 450 L 0 450 L 0 250">
         <animate
           attributeName="d"
           dur="5s"
-          values="M 0 250 C 600 100 650 450 1300 250 L 1300 350 L 0 350 L 0 250;
-                M 0 250 C 600 450 650 100 1300 250 L 1300 350 L 0 350 L 0 250;
-      M 0 250 C 600 100 650 450 1300 250 L 1300 350 L 0 350 L 0 250"
+          values="M 0 250 C 600 100 650 450 1300 250 L 1300 450 L 0 450 L 0 250;
+                M 0 250 C 600 450 650 100 1300 250 L 1300 450 L 0 450 L 0 250;
+      M 0 250 C 600 100 650 450 1300 250 L 1300 450 L 0 450 L 0 250"
           repeatCount="indefinite"
         />
       </path>
 
-      <path className="st1" d="M 0 250 C 950 350 950 150 1300 250 L 1300 350 L 0 350 L 0 250">
+      <path className="st1" d="M 0 250 C 950 450 950 150 1300 250 L 1300 450 L 0 450 L 0 250">
         <animate
           attributeName="d"
           dur="5s"
           begin="2s"
-          values="M 0 250 C 950 350 950 150 1300 250 L 1300 350 L 0 350 L 0 250;
-                M 0 250 C 350 150 350 350 1300 250 L 1300 350 L 0 350 L 0 250;
-        M 0 250 C 950 350 950 150 1300 250 L 1300 350 L 0 350 L 0 250"
+          values="M 0 250 C 950 450 950 150 1300 250 L 1300 450 L 0 450 L 0 250;
+                M 0 250 C 450 150 450 450 1300 250 L 1300 450 L 0 450 L 0 250;
+        M 0 250 C 950 450 950 150 1300 250 L 1300 450 L 0 450 L 0 250"
           repeatCount="indefinite"
         />
       </path>
 
-      <path className="st1" d="M 0 250 C 350 150 350 350 1300 250 L 1300 350 L 0 350 L 0 250">
+      <path className="st1" d="M 0 250 C 450 150 450 450 1300 250 L 1300 450 L 0 450 L 0 250">
         <animate
           attributeName="d"
           dur="5s"
-          values="M 0 250 C 350 150 350 350 1300 250 L 1300 350 L 0 350 L 0 250;
-                M 0 250 C 950 350 950 150 1300 250 L 1300 350 L 0 350 L 0 250;
-        M 0 250 C 350 150 350 350 1300 250 L 1300 350 L 0 350 L 0 250"
+          values="M 0 250 C 450 150 450 450 1300 250 L 1300 450 L 0 450 L 0 250;
+                M 0 250 C 950 450 950 150 1300 250 L 1300 450 L 0 450 L 0 250;
+        M 0 250 C 450 150 450 450 1300 250 L 1300 450 L 0 450 L 0 250"
           repeatCount="indefinite"
         />
       </path>
