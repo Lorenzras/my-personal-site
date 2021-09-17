2765f3e83ffbcd4afca5ef373d3a81e624b89a70
Lorenz Ras <lenzras@gmail.com>
2021-09-18 07:01:46 +0900

    updated card colors

--- src/components/containers/SkillsCard.jsx
+++ src/components/containers/SkillsCard.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 
 const SkillsCard = ({ children }) => (
-  <div className="p-6 w-full min-h-32 max-h-48 mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+  <div className="p-6 w-full min-h-32 max-h-48 mx-auto rounded-xl shadow-md flex items-center space-x-4 bg-gradient-to-r from-white to-yellow-50">
     {children}
   </div>
 );
