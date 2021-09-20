c3e3fe0850e55e972e948dbeda09c59a690ac52e
Lorenz Ras <lenzras@gmail.com>
2021-09-20 20:44:41 +0900

    added GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,25 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-google-analytics',
       options: {
-        trackingId: '286023078',
+        // The property ID; the tracking code won't be generated without it
+        trackingId: 'G-KL34P7JY0F',
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ['/preview/**', '/do-not-track/me/too/'],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        defer: false,
+        // Any additional optional fields
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: 'example.com',
+        // defaults to false
+        enableWebVitalsTracking: true,
       },
     },
     'gatsby-plugin-react-helmet',
