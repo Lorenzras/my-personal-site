45fa277c3871a6e618afd6fd56b2d42efa86dc19
Lorenz Ras <lenzras@gmail.com>
2021-09-22 06:47:11 +0900

    Update useOnScreen.js

--- src/hooks/useOnScreen.js
+++ src/hooks/useOnScreen.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 
-const useOnScreen = (ref, rootMargin = '-300px') => {
+const useOnScreen = (ref, rootMargin = '-100px') => {
   // State and setter for storing whether element is visible
   const [isIntersecting, setIntersecting] = useState(false);
   useEffect(() => {
